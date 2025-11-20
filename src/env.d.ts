85b8c954fcead67d342b82ecc8b6e6797f5de1c7
Jairo Sánchez <jairosanchezb5@gmail.com>
2025-11-20 13:58:48 +0100

    Agregar interfaces para las variables de entorno de las APIs de clima y geocodificación

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_WEATHER_API_URL: string;
+  readonly PUBLIC_GEOCODING_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
